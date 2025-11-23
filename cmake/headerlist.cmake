b847f8724ff0c0456b7ef0eba29e0b98ac455b9b
Viny <77700518+vinymayan@users.noreply.github.com>
2025-11-23 14:53:57 -0300

    bo

---
+++ cmake/headerlist.cmake
@@ -0,0 +1,25 @@
+set(headers ${headers}
+	src/API.h
+
+src/CPR.h
+
+src/events.h
+
+src/hooks.h
+
+src/offsets.h
+
+src/PCH.h
+
+src/PayloadHandlerManager.h
+
+src/PayloadInterpreter.h
+
+src/Utils.h
+
+src/payloadHandlers/payloadHandler.h
+
+src/payloadManager.h
+
+src/settings.h
+)
---
+++ cmake/sourcelist.cmake
@@ -0,0 +1,29 @@
+set(sources ${sources}
+	src/CPR.cpp
+
+src/PayloadInterpreter.cpp
+
+src/Utils.cpp
+
+src/main.cpp
+
+src/payloadHandlers/actorValueHandler.cpp
+
+src/payloadHandlers/cameraHandler.cpp
+
+src/payloadHandlers/CPRHandler.cpp
+
+src/payloadHandlers/globalTimeHandler.cpp
+
+src/payloadHandlers/graphVariableHandler.cpp
+
+src/payloadHandlers/particleHandler.cpp
+
+src/payloadHandlers/setGhostHandler.cpp
+
+src/payloadHandlers/spellApplyHandler.cpp
+
+src/payloadHandlers/spellCastHandler.cpp
+
+src/payloadManager.cpp
+)
